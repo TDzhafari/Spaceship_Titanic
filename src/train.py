02dd99da75098db722974bcc57c2a6b9ab3b8f04
Timur <45837908+TDzhafari@users.noreply.github.com>
2026-03-13 19:49:38 -0400

    updated folder structure

---
+++ src/train.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix, classification_report
+
+#######################################################################################################
+#   Project: Spaceship Titatic (Train)
+#   Author: Timur Dzhafari
+#   Date: (TBD)
+#
+#   Description: 
+#######################################################################################################
+
+
+#######################################################################################################
+#                   Train Model
+#######################################################################################################
+
+def train_dtree():
+    pass
+
+def train_rf():
+    pass
+
+def train_xgb():
+    pass
+
+#######################################################################################################
+#                   Validate
+#######################################################################################################
+
+#######################################################################################################
+#                   Run Prediction
+#######################################################################################################
